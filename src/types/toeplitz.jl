e6926834fad870fb8f01fa9cef29d90054d495d6
OskarBrzeski <hscs15@durham.ac.uk>
2023-02-02 21:43:51 +0000

    Changed how data is stored in Toeplitz struct

--- src/types/toeplitz.jl
+++ src/types/toeplitz.jl
@@ -3,24 +3,24 @@ import Base.getindex
 import Base.length
 import Base.size
 
-
 struct Toeplitz{T, V<:AbstractVector{T}} <: AbstractMatrix{T}
+    tr::T
     row::V
     col::V
-    function Toeplitz{T, V}(row, col) where {T, V<:AbstractVector{T}}
+    function Toeplitz{T, V}(tr, row, col) where {T, V<:AbstractVector{T}}
         Base.require_one_based_indexing(row, col)
-        new{T, V}(row, col)
+        new{T, V}(tr, row, col)
     end
 end
 
 # Alternate Constructers
 
-Toeplitz(row::V, col::V) where {T, V<:AbstractVector{T}} = Toeplitz{T}(row, col)
-Toeplitz{T}(row::V, col::V) where {T, V<:AbstractVector{T}} = Toeplitz{T, V}(row, col)
+Toeplitz(tr::T, row::V, col::V) where {T, V<:AbstractVector{T}} = Toeplitz{T}(tr, row, col)
+Toeplitz{T}(tr::T, row::V, col::V) where {T, V<:AbstractVector{T}} = Toeplitz{T, V}(tr, row, col)
 
 # Basic Functions
 
-size(A::Toeplitz) = (length(A.col) + 1, length(A.row))
+size(A::Toeplitz) = (length(A.col) + 1, length(A.row) + 1)
 function size(A::Toeplitz, d::Integer)
     if d < 0
         throw("dimesntion must be ≥ 1, got $d")
@@ -38,9 +38,9 @@ axes(A::Toeplitz, d::Integer) = Base.OneTo(size(A, d))
 
 function getindex(A::Toeplitz{T}, i::Integer, j::Integer) where T
     if i == j
-        return A.row[1]
+        return A.tr
     elseif i < j
-        return A.row[j - i + 1]
+        return A.row[j - i]
     else
         return A.col[i - j]
     end
@@ -48,9 +48,9 @@ end
 
 function setindex!(A::Toeplitz{T}, x, i::Integer, j::Integer) where T
     if i == j
-        A.row[0] = x
+        A.tr = x
     elseif i < j
-        A.row[j - i + 1] = x
+        A.row[j - i] = x
     else
         A.col[i - j] = x
     end
@@ -58,15 +58,15 @@ end
 
 # Matrix related calculations
 
-transpose(A::Toeplitz) = Toeplitz([A.row[1], A.col...], A.row[2:end])
+transpose(A::Toeplitz) = Toeplitz(A.tr, A.col, A.row)
 
 adjoint(A::Toeplitz{<:Real}) = transpose(A)
 function adjoint(A::Toeplitz)
-    row = map(conj, [A.row[1], A.col...])
-    col = map(conj, A.row[2:end])
-    return Toeplitz(row, col)
+    tr = conj(A.tr)
+    row = map(conj, A.col)
+    col = map(conj, A.row)
+    return Toeplitz(tr, row, col)
 end
 
-
-issymmetric(A::Toeplitz) = A == transpose(A)
-ishermitian(A::Toeplitz) = A == adjoint(A)
+issymmetric(A::Toeplitz) = A.row == A.col
+ishermitian(A::Toeplitz) = A.row == map(conj, A.col)
