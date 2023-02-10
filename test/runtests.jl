b48f31c5b32a163b65d71aa724c3696ef7f551b3
OskarBrzeski <hscs15@durham.ac.uk>
2023-02-10 02:52:58 +0000

    Updated Testing Infrastructure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
 using SafeTestsets
 
-@safetestset "toeplitz.jl tests" begin include("toeplitz_tests.jl") end
+@safetestset "toeplitz.jl tests" begin include("type_tests/toeplitz_tests.jl") end
+@safetestset "circulant.jl tests" begin include("type_tests/circulant_tests.jl") end
