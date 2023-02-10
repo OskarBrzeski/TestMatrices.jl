ada12d3f51361fa436f3e2999441a87c8ef855b5
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-02-10 15:19:30 +0000

    Added Toeplitz generator

--- src/generators/toeplitz.jl
+++ src/generators/toeplitz.jl
@@ -12,3 +12,10 @@ function rand_symmetric_toeplitz(range::AbstractRange, m::Integer)
     row = rand(range, m - 1)
     return Toeplitz(tl, row, row)
 end
+
+function rand_sequence_toeplitz(sequence::AbstractVector, m::Integer, n::Integer)
+    tl = rand(sequence)
+    row = rand(sequence, n - 1)
+    col = rand(sequence, m - 1)
+    return Toeplitz(tl, row, col)
+end
