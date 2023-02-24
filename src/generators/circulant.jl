99489f1d03148601bcb2c09033424c99e512ec34
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-02-24 02:47:53 +0000

    altered generators

--- src/generators/circulant.jl
+++ src/generators/circulant.jl
@@ -1,14 +1,4 @@
 import TestMatrices.Circulant
 
-rand_range_circulant(range::AbstractRange, m::Integer) = Circulant(rand(range, m))
-
-function rand_symmetric_circulant(range::AbstractRange, m::Integer)
-    tl = rand(range)
-    row = rand(range, m÷2)
-    if m % 2 == 0
-        fullrow = [tl, row..., reverse(row[1:end-1])...]
-    else
-        fullrow = [tl, row..., reverse(row)...]
-    end
-    return Circulant(fullrow)
-end
+generate(::Type{Circulant}, range::AbstractRange, m::Integer) = Circulant(rand(range, m))
+generate(::Type{Circulant}, sequence::AbstractArray, m::Integer) = Circulant(rand(sequence, m))
