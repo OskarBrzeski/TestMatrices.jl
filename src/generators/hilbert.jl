2e995976f2410356855761cead322a05e49afa4a
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-03-17 02:13:16 +0000

    Remove redundant parameters

--- src/generators/hilbert.jl
+++ src/generators/hilbert.jl
@@ -1,4 +1,4 @@
 import TestMatrices.Hilbert
 
-generate(::Type{Hilbert}, range::AbstractRange, m::Integer) = Hilbert(rand(range))
-generate(::Type{Hilbert}, sequence::AbstractArray, m::Integer) = Hilbert(rand(sequence))
+generate(::Type{Hilbert}, range::AbstractRange) = Hilbert(rand(range))
+generate(::Type{Hilbert}, sequence::AbstractArray{Integer}) = Hilbert(rand(sequence))
