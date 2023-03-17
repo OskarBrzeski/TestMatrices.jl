36e37bb23f776815ed7a38befebefd85a0a22f84
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-03-17 02:23:19 +0000

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using SafeTestsets
 
 @safetestset "toeplitz.jl tests" begin include("type_tests/toeplitz_tests.jl") end
 @safetestset "circulant.jl tests" begin include("type_tests/circulant_tests.jl") end
+@safetestset "hilbert.jl tests" begin include("type_tests/hilbert_tests.jl") end
