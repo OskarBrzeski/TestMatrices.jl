3794b5dbe333cce3b33895fe191c97cd2a1297de
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2023-03-17 02:07:05 +0000

    Update Package

--- src/TestMatrices.jl
+++ src/TestMatrices.jl
@@ -2,9 +2,13 @@ module TestMatrices
 
 include("types/toeplitz.jl")
 include("types/circulant.jl")
+include("types/hankel.jl")
+include("types/hilbert.jl")
 
 export Toeplitz
 export Circulant
+export Hankel
+export Hilbert
 
 export issymmetric
 export ishermitian
